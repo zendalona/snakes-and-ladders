8e68c512d5800f58c903247aedf38ebdc985728a
VSarchana <archanavs1211@gmail.com>
2023-07-22 11:17:16 +0530

    setup script updated

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(name='SnakeAndLadder',
       license = 'GPL-3',
       packages=['SnakeAndLadder'],
       data_files=[('share/SnakeAndLadder/',['data/icon.png' ]),
-      ('share/SnakeAndLadder/sounds/',['data/sounds/dice_sound.wav', 'data/sounds/got_promotion.ogg', 'data/sounds/next_level_6.ogg' ,  'data/sounds/start.ogg' ,  'data/sounds/start.wav' , 'data/sounds/warning.ogg' ,  'data/sounds/wrong_pressed.ogg']),
+      ('share/SnakeAndLadder/sounds/',['share/SnakeAndLadder/sounds/dice_sound.wav', 'share/SnakeAndLadder/sounds/win.wav', ' share/SnakeAndLadder/sounds/climb-1.ogg' ,'share/SnakeAndLadder/sounds/climb-2.ogg', 'share/SnakeAndLadder/sounds/climb-3.ogg','share/SnakeAndLadder/sounds/fall-1.ogg','share/SnakeAndLadder/sounds/fall-2.ogg','share/SnakeAndLadder/sounds/fall-3.ogg','share/SnakeAndLadder/sounds/correct-1.ogg','share/SnakeAndLadder/sounds/correct-2.ogg', 'share/SnakeAndLadder/sounds/start.wav' ,'share/SnakeAndLadder/sounds/wrong-anwser-1.ogg','share/SnakeAndLadder/sounds/wrong-anwser-2.ogg','share/SnakeAndLadder/sounds/wrong-anwser-3.ogg' ]),
       ('share/applications/',['snake-and-ladder.desktop']),
       ('bin/',['snake-and-ladder'])]
       )
